15af8e1eabaa8165d8b804add35fe2023c018234
Scott Starks <esstarks@hotmail.com>
2026-04-08 10:04:58 -0600

    setup project with fileserver and index.html

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/thetramp22/chirpy_bd
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	srv := &http.Server{
+		Handler: mux,
+		Addr:    ":" + port,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+}
