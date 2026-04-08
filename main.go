9f1e3d500fce705329e6061a43fb2d770ec41590
Scott Starks <esstarks@hotmail.com>
2026-04-08 17:44:28 -0600

    added a /healthz endpoint

--- main.go
+++ main.go
@@ -10,7 +10,12 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
 	srv := &http.Server{
 		Handler: mux,
 		Addr:    ":" + port,
